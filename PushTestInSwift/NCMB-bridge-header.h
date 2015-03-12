ea8a016554fd728751b01b3b726ffc0c36b85c25
Yuki Okawa <okawa.yuki@nifty.co.jp>
2015-03-12 20:21:34 +0900

    initial commit

---
+++ PushTestInSwift/NCMB-bridge-header.h
@@ -0,0 +1,14 @@
+//
+//  NCMB-bridge-header.h
+//  PushTestInSwift
+//
+//  Created by SCI01433 on 2015/03/12.
+//  Copyright (c) 2015年 OkawaUki. All rights reserved.
+//
+
+#ifndef PushTestInSwift_NCMB_bridge_header_h
+#define PushTestInSwift_NCMB_bridge_header_h
+
+#import <NCMB/NCMB.h>
+
+#endif
---
+++ PushTestInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushTestInSwift
+//
+//  Created by SCI01433 on 2015/03/12.
+//  Copyright (c) 2015年 OkawaUki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
